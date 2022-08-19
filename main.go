b54f0b7f12ebaf382c129ba5d0f2000689479f07
aratama <16192627+aratama@users.noreply.github.com>
2022-08-19 22:06:12 +0900

    planetscale

--- main.go
+++ main.go
@@ -2,18 +2,35 @@ package main
 
 import (
 	"database/sql"
+	"log"
 	"net/http"
+	"os"
 
 	"aratama.github.com/go-gin-todo/todo"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
 	"modernc.org/sqlite"
 )
 
 func main() {
-	// https://github.com/ent/ent/issues/2460
-	sql.Register("sqlite3", &sqlite.Driver{})
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 
-	router := todo.InitizeServer("sqlite3", "todo.sqlite?_pragma=foreign_keys(1)")
-	router.Run("localhost:8080")
+	useSqlite := true
+
+	if useSqlite {
+		// https://github.com/ent/ent/issues/2460
+		sql.Register("sqlite3", &sqlite.Driver{})
+		router := todo.InitizeServer("sqlite3", "todo.sqlite?_pragma=foreign_keys(1)")
+		router.Run("localhost:8080")
+	} else {
+		// NOte: need parseTime=true option in dataSouce
+		router := todo.InitizeServer("mysql", os.Getenv("DSN"))
+		router.Run("localhost:8080")
+	}
+
 }
 
 func ServeForCloudFunctions(w http.ResponseWriter, r *http.Request) {
